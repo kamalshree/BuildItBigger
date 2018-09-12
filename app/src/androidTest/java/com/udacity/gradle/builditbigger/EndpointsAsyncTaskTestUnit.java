9a785da1c47fd4b0428e81be79abe48d80f60e8e
codesqills <codesqills@gmail.com>
2018-09-11 23:49:17 -0500

    Updated the test for the async task

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/EndpointsAsyncTaskTestUnit.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/EndpointsAsyncTaskTestUnit.java
@@ -1,33 +1,29 @@
 package com.udacity.gradle.builditbigger;
 
-import android.support.test.InstrumentationRegistry;
-import android.support.test.filters.LargeTest;
+import android.support.test.espresso.intent.Intents;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.test.AndroidTestCase;
 
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import com.android.jokesandroidlib.MyJokesActivity;
 
-
-import java.util.concurrent.TimeUnit;
-
-import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 /**
  * Created by kamalshree on 9/7/2018.
  */
 
 @RunWith(AndroidJUnit4.class)
-@LargeTest
 
-public class EndpointsAsyncTaskTestUnit extends AndroidTestCase {
+public class EndpointsAsyncTaskTestUnit{
+    String TAG = EndpointsAsyncTask.class.getSimpleName();
     @Rule
     public ActivityTestRule<MainActivity> mActivityRule =
             new ActivityTestRule(MainActivity.class);
@@ -35,18 +31,10 @@ public class EndpointsAsyncTaskTestUnit extends AndroidTestCase {
 
 
     @Test
-    public void IsNotEmpty() throws Exception {
-
-        EndpointsAsyncTaskTest asyncTest =  new EndpointsAsyncTaskTest();
-        asyncTest.execute(InstrumentationRegistry.getContext());
-        String joke = asyncTest.get(10, TimeUnit.SECONDS);
-        Assert.assertTrue(!joke.equals(""));
-    }
-
-    @Test
-    public void testResponse() {
+    public void onButtonClicked() {
+        Intents.init();
         onView(withId(R.id.button)).perform(click());
-        onView(withId(R.id.tv_jokes)).check(matches(isDisplayed()));
+        intended(hasComponent(MyJokesActivity.class.getName()));
+        Intents.release();
     }
-
 }
